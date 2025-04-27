c4077153d895f6779c9b26d0e69327faca1983ff
sumeyyesulesahin <117784053+sumeyyesulesahin@users.noreply.github.com>
2025-04-27 03:33:26 +0300

    Add files via upload

---
+++ Odev2/app/src/main/java/com/example/odev2/Odev2/Odev2.kt
@@ -0,0 +1,38 @@
+package com.example.odev2.Odev2
+
+
+// fahrenhiet a donusturme
+fun main() {
+    fun fahrenheit(celsius: Double): Double {
+        return celsius * 1.8 + 32
+    }
+
+    // dikdortgen çevresi hesaaplama
+    fun dikdortgen(en :Double, boy:Double):Double{
+        return (2*en)+(2*boy)
+    }
+
+    // faktoriyel hesaplama
+
+    fun faktoriyel(sayi: Int): Int {
+        var sonuc = 1
+        for (i in 1..sayi) {
+            sonuc *= i
+        }
+        return sonuc
+    }
+
+    // a harfi kontrolu
+    fun aHarfi(kelime: String): Int {
+        var sayac = 0
+        for (harf in kelime) {
+            if (harf == 'a' || harf == 'A') {
+                sayac++
+            }
+        }
+        return sayac
+    }
+
+
+
+}
