b8334226d226497d08faab03c25e02647b06ac37
sumeyye <sumeyyesule1141@gmail.com>
2025-04-27 03:23:28 +0300

    Değişiklik açıklaması

--- app/src/main/java/com/example/odev/OdevDosyasi/Odev2.kt
+++ app/src/main/java/com/example/odev/OdevDosyasi/Odev2.kt
@@ -1,5 +1,5 @@
 package com.example.odev.OdevDosyasi
- // fahrenhiet a donusturme
+// fahrenhiet a donusturme
 fun main() {
     fun fahrenheit(celsius: Double): Double {
         return celsius * 1.8 + 32
@@ -10,27 +10,27 @@ fun main() {
         return (2*en)+(2*boy)
     }
 
-     // faktoriyel hesaplama
+    // faktoriyel hesaplama
 
-     fun faktoriyel(sayi: Int): Int {
-         var sonuc = 1
-         for (i in 1..sayi) {
-             sonuc *= i
-         }
-         return sonuc
-     }
+    fun faktoriyel(sayi: Int): Int {
+        var sonuc = 1
+        for (i in 1..sayi) {
+            sonuc *= i
+        }
+        return sonuc
+    }
 
-     // a harfi kontrolu
-     fun aHarfi(kelime: String): Int {
-         var sayac = 0
-         for (harf in kelime) {
-             if (harf == 'a' || harf == 'A') {
-                 sayac++
-             }
-         }
-         return sayac
-     }
+    // a harfi kontrolu
+    fun aHarfi(kelime: String): Int {
+        var sayac = 0
+        for (harf in kelime) {
+            if (harf == 'a' || harf == 'A') {
+                sayac++
+            }
+        }
+        return sayac
+    }
 
 
 
- }
+}
